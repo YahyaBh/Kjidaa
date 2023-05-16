dfc3f56a6afbd930a13603a58186d55d87009bd3
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-05-16 12:11:42 +0100

    Push everything last

--- client_side/src/Components/Gallery/Gallery.jsx
+++ client_side/src/Components/Gallery/Gallery.jsx
@@ -28,165 +28,174 @@ const Gallery = () => {
 
 
                 <section>
+
+                    <h2>Nos travailles <span>précédents</span></h2>
+
                     <div className="parent">
 
-                        <h2>Nos travailles précédents</h2>
 
                         <div className="div1">
                             <img src="./Images_public/Gallery/IMG-20230428-WA0001.jpg" alt="IMAGE1" />
                         </div>
                         <div className="div2">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0002.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0002.jpg" alt="IMAGE2" />
                         </div>
                         <div className="div3">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0003.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0003.jpg" alt="IMAGE3" />
                         </div>
                         <div className="div4">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0004.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0004.jpg" alt="IMAGE4" />
                         </div>
                         <div className="div5">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0005.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0005.jpg" alt="IMAGE5" />
                         </div>
                         <div className="div6">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0006.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0006.jpg" alt="IMAGE6" />
                         </div>
                         <div className="div7">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0007.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0007.jpg" alt="IMAGE7" />
                         </div>
                         <div className="div8">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0008.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0008.jpg" alt="IMAGE8" />
                         </div>
                         <div className="div9">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0009.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0009.jpg" alt="IMAGE9" />
                         </div>
                         <div className="div10">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0010.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0010.jpg" alt="IMAGE10" />
                         </div>
                         <div className="div11">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0011.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0011.jpg" alt="IMAGE11" />
                         </div>
                         <div className="div12">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0012.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0012.jpg" alt="IMAGE12" />
                         </div>
                         <div className="div13">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE13" />
+                        </div>
+                        <div className="div13l">
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE14" />
                         </div>
                         <div className="div14">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0014.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0014.jpg" alt="IMAGE15" />
                         </div>
                         <div className="div15">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0015.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0015.jpg" alt="IMAGE16" />
                         </div>
                         <div className="div16">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0016.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0016.jpg" alt="IMAGE17" />
                         </div>
                         <div className="div17">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0017.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0017.jpg" alt="IMAGE18" />
                         </div>
                         <div className="div18">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0018.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0018.jpg" alt="IMAGE19" />
                         </div>
                         <div className="div19">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0019.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0019.jpg" alt="IMAGE20" />
                         </div>
                         <div className="div20">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0020.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0020.jpg" alt="IMAGE21" />
                         </div>
                         <div className="div21">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0021.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0021.jpg" alt="IMAGE22" />
                         </div>
                         <div className="div22">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0022.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0022.jpg" alt="IMAGE23" />
                         </div>
                         <div className="div23">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0023.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0023.jpg" alt="IMAGE24" />
                         </div>
                         <div className="div24">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0024.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0024.jpg" alt="IMAGE25" />
                         </div>
                         <div className="div25">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0025.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0025.jpg" alt="IMAGE26" />
                         </div>
                     </div>
 
+
+                    <h2>Organisation des <span>évènements</span></h2>
+
                     <div className="parent">
 
-                        <h2>Organisation des évènements</h2>
-
                         <div className="div1">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0001.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0026.jpg" alt="IMAGE1" />
                         </div>
                         <div className="div2">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0002.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0027.jpg" alt="IMAGE2" />
                         </div>
                         <div className="div3">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0003.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0028.jpg" alt="IMAGE3" />
                         </div>
                         <div className="div4">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0004.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0029.jpg" alt="IMAGE4" />
                         </div>
                         <div className="div5">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0005.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0030.jpg" alt="IMAGE5" />
                         </div>
                         <div className="div6">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0006.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0031.jpg" alt="IMAGE6" />
                         </div>
                         <div className="div7">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0007.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0032.jpg" alt="IMAGE7" />
                         </div>
                         <div className="div8">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0008.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0033.jpg" alt="IMAGE8" />
                         </div>
                         <div className="div9">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0009.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0034.jpg" alt="IMAGE9" />
                         </div>
                         <div className="div10">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0010.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0035.jpg" alt="IMAGE10" />
                         </div>
                         <div className="div11">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0011.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0036.jpg" alt="IMAGE11" />
                         </div>
                         <div className="div12">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0012.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0037.jpg" alt="IMAGE12" />
                         </div>
                         <div className="div13">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0038.jpg" alt="IMAGE13" />
+                        </div>
+                        <div className="div13l">
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0039.jpg" alt="IMAGE14" />
                         </div>
                         <div className="div14">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0014.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0040.jpg" alt="IMAGE15" />
                         </div>
                         <div className="div15">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0015.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0041.jpg" alt="IMAGE16" />
                         </div>
                         <div className="div16">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0016.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0042.jpg" alt="IMAGE17" />
                         </div>
                         <div className="div17">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0017.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0043.jpg" alt="IMAGE18" />
                         </div>
                         <div className="div18">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0018.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0044.jpg" alt="IMAGE19" />
                         </div>
                         <div className="div19">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0019.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0045.jpg" alt="IMAGE20" />
                         </div>
                         <div className="div20">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0020.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0046.jpg" alt="IMAGE21" />
                         </div>
                         <div className="div21">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0021.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0047.jpg" alt="IMAGE22" />
                         </div>
                         <div className="div22">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0022.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0048.jpg" alt="IMAGE23" />
                         </div>
                         <div className="div23">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0023.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0049.jpg" alt="IMAGE24" />
                         </div>
                         <div className="div24">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0024.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0050.jpg" alt="IMAGE25" />
                         </div>
                         <div className="div25">
-                            <img src="./Images_public/Gallery/IMG-20230428-WA0025.jpg" alt="IMAGE1" />
+                            <img src="./Images_public/Gallery/IMG-20230428-WA0051.jpg" alt="IMAGE26" />
                         </div>
                     </div>
                 </section>
