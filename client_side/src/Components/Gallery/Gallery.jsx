04b8a34003e508713eacbee18c98e4d05af25525
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2023-05-17 09:52:08 +0100

    fixing pics

--- client_side/src/Components/Gallery/Gallery.jsx
+++ client_side/src/Components/Gallery/Gallery.jsx
@@ -70,79 +70,88 @@ const Gallery = () => {
                             <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0001.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0001.jpg" alt="IMAGE1" />
                         </div>
                         <div className="div2">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0002.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0002.jpg" alt="IMAGE2" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0005.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0005.jpg" alt="IMAGE2" />
                         </div>
                         <div className="div3">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0003.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0003.jpg" alt="IMAGE3" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0006.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0006.jpg" alt="IMAGE3" />
                         </div>
                         <div className="div4">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0004.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0004.jpg" alt="IMAGE4" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0007.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0007.jpg" alt="IMAGE4" />
                         </div>
                         <div className="div5">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0005.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0005.jpg" alt="IMAGE5" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0008.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0008.jpg" alt="IMAGE5" />
                         </div>
                         <div className="div6">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0006.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0006.jpg" alt="IMAGE6" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0009.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0009.jpg" alt="IMAGE6" />
                         </div>
                         <div className="div7">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0007.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0007.jpg" alt="IMAGE7" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0010.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0010.jpg" alt="IMAGE7" />
                         </div>
                         <div className="div8">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0008.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0008.jpg" alt="IMAGE8" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0011.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0011.jpg" alt="IMAGE8" />
                         </div>
                         <div className="div9">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0009.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0009.jpg" alt="IMAGE9" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0012.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0012.jpg" alt="IMAGE9" />
                         </div>
                         <div className="div10">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0010.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0010.jpg" alt="IMAGE10" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0014.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0014.jpg" alt="IMAGE10" />
                         </div>
                         <div className="div11">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0011.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0011.jpg" alt="IMAGE11" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0029.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0029.jpg" alt="IMAGE11" />
                         </div>
                         <div className="div12">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0012.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0012.jpg" alt="IMAGE12" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0040.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0040.jpg" alt="IMAGE12" />
                         </div>
                         <div className="div13">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0013.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE13" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0041.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0041.jpg" alt="IMAGE13" />
                         </div>
                         <div className="div13l">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0013.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0013.jpg" alt="IMAGE14" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0042.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0042.jpg" alt="IMAGE14" />
                         </div>
                         <div className="div14">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0014.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0014.jpg" alt="IMAGE15" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0045.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0045.jpg" alt="IMAGE15" />
                         </div>
                         <div className="div15">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0015.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0015.jpg" alt="IMAGE16" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0046.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0046.jpg" alt="IMAGE16" />
                         </div>
                         <div className="div16">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0016.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0016.jpg" alt="IMAGE17" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0047.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0047.jpg" alt="IMAGE17" />
                         </div>
                         <div className="div17">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0017.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0017.jpg" alt="IMAGE18" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0048.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0048.jpg" alt="IMAGE18" />
                         </div>
                         <div className="div18">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0018.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0018.jpg" alt="IMAGE19" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0049.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0049.jpg" alt="IMAGE19" />
                         </div>
                         <div className="div19">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0019.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0019.jpg" alt="IMAGE20" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0050.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0050.jpg" alt="IMAGE20" />
                         </div>
                         <div className="div20">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0020.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0020.jpg" alt="IMAGE21" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0051.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0051.jpg" alt="IMAGE21" />
                         </div>
                         <div className="div21">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0021.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0021.jpg" alt="IMAGE22" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0052.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0052.jpg" alt="IMAGE22" />
                         </div>
                         <div className="div22">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0022.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0022.jpg" alt="IMAGE23" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0053.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0053.jpg" alt="IMAGE23" />
                         </div>
                         <div className="div23">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0023.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0023.jpg" alt="IMAGE24" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0054.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0054.jpg" alt="IMAGE24" />
                         </div>
                         <div className="div24">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0024.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0024.jpg" alt="IMAGE25" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0055.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0055.jpg" alt="IMAGE25" />
                         </div>
                         <div className="div25">
-                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0025.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0025.jpg" alt="IMAGE26" />
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0056.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0056.jpg" alt="IMAGE26" />
+                        </div>
+                        <div className="div26">
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0064.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0064.jpg" alt="IMAGE27" />
+                        </div>
+                        <div className="div27">
+                            <img onClick={e => zoomImage('./Images_public/Gallery/IMG-20230428-WA0077.jpg')} src="./Images_public/Gallery/IMG-20230428-WA0077.jpg" alt="IMAGE28" />
+                        </div>
+                        <div className="div28">
+                            <img onClick={e => zoomImage('./Images_public/Gallery/WhatsApp Image 2023-05-15 at 21.23.33.jpg')} src="./Images_public/Gallery/WhatsApp Image 2023-05-15 at 21.23.33.jpg" alt="IMAGE29" />
                         </div>
                     </div>
 
